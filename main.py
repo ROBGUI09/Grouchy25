90d3994db1304b8b5edc923d61cab8159eef85d6
Роберт <pobept46.2002@gmail.com>
2022-07-16 21:18:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ async def howru(ctx):
 	
 @bot.command(name="8ball")
 async def ball(ctx, *arg):
-	embed=discord.Embed(title=f"{ctx.message.author.mention} спросил у магического шара:", description=" ".join(arg))
+	embed=discord.Embed(description=f"{ctx.message.author.mention} спросил у магического шара: **"+" ".join(arg)+"**")
 	embed.add_field(name="Шар ответил:", value=random.choice(ballanswers), inline=False)
 	embed.set_footer(icon_url="https://cdn.discordapp.com/attachments/932191860712177664/997799046238457956/unknown.png", text="by Grouchy and Kelk")
 	await ctx.message.reply(embed=embed)
