1f3a0911336fe9c1f6f6cde6775078f1abd41133
Роберт <pobept46.2002@gmail.com>
2022-07-16 21:09:12 +0500

    Update data.py

--- data.py
+++ data.py
@@ -30,3 +30,30 @@ ballanswers = [
 
 
 howrutexts=["Сойдет","Могло быть лучше...","Отлично","Нормально","Наверное нормально..."]
+
+hellotexts=["Здарова","Привет","Привет, как дела?","Здрасте",
+	       "Hello (English)",
+	       "Ahoj (Czech)",
+	       "Halo (Indonesian)",
+	       "Cześć (Polish)",
+	       "Hej (Danish)",
+	       "Merhaba (Turkish)",
+	       "Yassas (Greek)",
+	       "Ciao (Italian)",
+	       "Olá (Portuguese)",
+	       "Hallå (Swedish)",
+	       "Bonjour (French)",
+	       "Aloha (Hawaiian)",
+	       "Kon’nichiwa (Japanese)",
+	       "Zdrastĭ (Bulgarian)",
+	       "Namaste (Hindi)",
+	       "Sawubona (Zulu)",
+	       "Hola (Spanish)",
+	       "Nĭ Hăo (Mandarin)",
+	       "Xin chào (Vietnamese)",
+	       "Annyeonghaseyo (Korean)",
+	       "Sveiki (Lithuanian)",
+	       "Kaixo (Basque)",
+	       "Hallo (German)",
+	       "Hei (Finnish)",
+	       "Hujambo (Swahili)"]
