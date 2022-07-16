91aac11ba00aea2ac0674eca138716ee707eb797
Роберт <pobept46.2002@gmail.com>
2022-07-17 00:07:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import random
 from data import *
 from pogoda import get_weather, NotFoundError
-import youtube_dl
+import music
 import os
 
 
@@ -155,72 +155,7 @@ async def weather(ctx, *args):
 		await ctx.message.reply(embed=embed)
 		
 
-@bot.command()
-async def play(ctx, url : str):
-	song_there = os.path.isfile("song.mp3")
-	try:
-		if song_there:
-			os.remove("song.mp3")
-	except PermissionError:
-		await ctx.send("Wait for the current playing music to end or use the 'stop' command")
-		return
-
-	voiceChannel = None
-	if ctx.message.author.voice is None:
-		await ctx.send("ты за меня придурка не держи, в войс зайди")
-		return
-	else:
-		voiceChannel = ctx.message.author.voice.channel 
-	await voiceChannel.connect()
-	voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-
-	ydl_opts = {
-		'format': 'bestaudio/best',
-		'postprocessors': [{
-			'key': 'FFmpegExtractAudio',
-			'preferredcodec': 'mp3',
-			'preferredquality': '192',
-		}],
-	}
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		ydl.download([url])
-	for file in os.listdir("./"):
-		if file.endswith(".mp3"):
-			os.rename(file, "song.mp3")
-	voice.play(discord.FFmpegPCMAudio("song.mp3"))
-
-
-@bot.command()
-async def leave(ctx):
-	voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-	if voice.is_connected():
-		await voice.disconnect()
-	else:
-		await ctx.send("The bot is not connected to a voice channel.")
-
-
-@bot.command()
-async def pause(ctx):
-	voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-	if voice.is_playing():
-		voice.pause()
-	else:
-		await ctx.send("Currently no audio is playing.")
-
-
-@bot.command()
-async def resume(ctx):
-	voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-	if voice.is_paused():
-		voice.resume()
-	else:
-		await ctx.send("The audio is not paused.")
-
-
-@bot.command()
-async def stop(ctx):
-	voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-	voice.stop()
+music.setup(bot)
 	
 bot.run(token)
 
