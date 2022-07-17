b0773cbd7e5338d1bb8d2dc414836a09aa04c08f
Роберт <pobept46.2002@gmail.com>
2022-07-17 13:28:11 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ cur = db.cursor()
     
 def check_for_vip(guild_id):
   for row in cur.execute('SELECT * FROM guilds WHERE id=?',(guild_id,)):
-    return row[1] > time.time()
+    return time.time()
   cur.execute("INSERT INTO guilds VALUES (?,0)",(guild_id,))
   cur.commit()
+  return 0
