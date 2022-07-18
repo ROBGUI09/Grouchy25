da6121f37a58568159b077fe1a4c738379e1f7d7
Роберт <pobept46.2002@gmail.com>
2022-07-18 18:15:52 +0500

    Update icon.py

--- icon.py
+++ icon.py
@@ -7,12 +7,16 @@ import asyncio
 class ServerIcon(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.days = schedule.every().day.at("08:00").do(asyncio.run(day))
-        self.nights = schedule.every().day.at("18:00").do(asyncio.run(night))
+        schedule.every().day.at("08:00").do(asyncio.run(day))
+        schedule.every().day.at("18:00").do(asyncio.run(night))
 
     def cog_unload(self):
         del self.days
         del self.nights
+        
+    @tasks.loop(seconds=1.0)
+    async def task(self):
+        schedule.run_pending()
 
     async def day(self):
         r = requests.get("https://cdn.discordapp.com/attachments/932191860712177664/998565760865681468/unknown.png")
