4a42fea8f7ca87c2597fcb76fb5b82b662c059b3
Роберт <pobept46.2002@gmail.com>
2022-07-18 18:04:55 +0500

    Update icon.py

--- icon.py
+++ icon.py
@@ -1,23 +1,23 @@
 from discord.ext import tasks, commands
 from datetime import datetime
 import requests
+import schedule
+import asyncio
 
 class ServerIcon(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.night.start()
-        self.day.start()
+        self.days = schedule.every().day.at("08:00").do(asyncio.run(day))
+        self.nights = schedule.every().day.at("18:00").do(asyncio.run(night))
 
     def cog_unload(self):
-        self.night.cancel()
-        self.day.cancel()
+        del self.days
+        del self.nights
 
-    @tasks.loop(time=datetime(8,0,0))
     async def day(self):
         r = requests.get("https://cdn.discordapp.com/attachments/932191860712177664/998565760865681468/unknown.png")
         await bot.edit_server(bot.get_guild(997404296909947001), icon=r.content)
         
-    @tasks.loop(time=datetime(18,0,0))
     async def night(self):
         r = requests.get("https://cdn.discordapp.com/attachments/932191860712177664/998565623045050368/unknown.png")
         await bot.edit_server(bot.get_guild(997404296909947001), icon=r.content)
