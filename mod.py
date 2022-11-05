c5e8f74e70f7c8cf8f91951d2713d984a5d5201a
Роберт <pobept46.2002@gmail.com>
2022-11-05 09:09:23 +0500

    Перевод mod.py на русский язык

--- mod.py
+++ mod.py
@@ -16,10 +16,10 @@ def prune(bot):
     @cmd.has_permissions(manage_messages=True)
     async def prune(ctx, amount=0):
         if amount == 0:
-            await ctx.send("Please specify the number of messages you want to delete!")
+            await ctx.send("Укажите количество сообщений для удаления.")
             await ctx.message.add_reaction(emoji='❌')
         elif amount <= 0:  # lower then 0
-            await ctx.send("The number must be bigger than 0!")
+            await ctx.send("Количество сообщений для удаления должно быть больше 0.")
             await ctx.message.add_reaction(emoji='❌')
         else:
             await ctx.message.add_reaction(emoji='✅')
@@ -32,10 +32,10 @@ def kick(bot):
     async def kick(ctx, member: discord.Member, *, reason=None):
         try:
             await member.kick(reason=reason)
-            await ctx.send(f'{member} has been kicked!')
+            await ctx.send(f'{member} был кикнут с сервера.')
             await ctx.message.add_reaction(emoji='✅')
         except Exception as failkick:
-            await ctx.send("Failed to kick: " + str(failkick))
+            await ctx.send("Не удалось кикнуть: " + str(failkick))
             await ctx.message.add_reaction(emoji='❌')
 
 
@@ -45,8 +45,8 @@ def ban(bot):
     async def ban(ctx, member: discord.Member, *, reason=None):
         try:
             await member.ban(reason=reason)
-            await ctx.send(f'{member} has been banned!')
+            await ctx.send(f'{member} был забанен.')
             await ctx.message.add_reaction(emoji='✅')
         except Exception as e:
-            await ctx.send("Failed to ban: " + str(e))
+            await ctx.send("Не удалось забанить: " + str(e))
             await ctx.message.add_reaction(emoji='❌')
