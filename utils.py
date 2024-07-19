2b5185742d6008691385ab679eb9bca2e2eb1370
Роберт <pobept46.2002@gmail.com>
2024-07-19 11:31:39 +0500

    Update utils.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- utils.py
+++ utils.py
@@ -5,11 +5,13 @@ db = sqlite3.connect('dbs/grouchy.db')
 cur = db.cursor()
     
 def check_for_vip(guild_id):
-  for row in cur.execute('SELECT * FROM guilds WHERE id=?',(guild_id,)):
-    return row[1]
-  cur.execute("INSERT INTO guilds VALUES (?,0)",(guild_id,))
-  db.commit()
-  return 0
+    cur.execute('SELECT vip_status FROM guilds WHERE id=?', (guild_id,))
+    row = cur.fetchone()
+    if row:
+        return row[0]
+    cur.execute("INSERT INTO guilds (id, vip_status) VALUES (?,0)", (guild_id,))
+    db.commit()
+    return 0
 
 
 import os
