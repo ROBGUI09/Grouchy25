ffed24c873a8e3ff94c9da4c0f39ed53c664c3dd
Роберт <pobept46.2002@gmail.com>
2024-07-19 05:47:08 +0500

    Update cogs/pogoda.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- cogs/pogoda.py
+++ cogs/pogoda.py
@@ -6,5 +6,4 @@ owm = OWM('6e0d73131fde98fc9c01f5ae5d5db3b1', config_dict)
 mgr = owm.weather_manager()
 
 def get_weather(city: str):
-	observation = mgr.weather_at_place(city)
-	return observation
+	return mgr.weather_at_place(city)
